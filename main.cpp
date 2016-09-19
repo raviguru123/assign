cd9b56bb7edb635bc0a4d9072ab61b9f695d778c
raviguru123 <raviguruiitr@gmail.com>
2016-09-19 18:09:29 +0530

    add delete functionality in hash map

--- main.cpp
+++ main.cpp
@@ -17,7 +17,23 @@ int hashKeyGeneration(string word)
     return result;
 }
 
-int searchNode(vector<node*> &v,int low,int high,int key)
+string convertInt(int number)
+{
+    if (number == 0)
+        return "0";
+    string temp="";
+    string returnvalue="";
+    while (number>0)
+    {
+        temp+=number%10+48;
+        number/=10;
+    }
+    for (int i=0; i<temp.length(); i++)
+        returnvalue+=temp[temp.length()-i-1];
+    return returnvalue;
+}
+
+int searchKey(vector<node*> &v,int low,int high,int key)
 {
     int mid=(low+high)/2;
     if(low>=high)
@@ -28,11 +44,11 @@ int searchNode(vector<node*> &v,int low,int high,int key)
     {
         if(v[mid]->key<key)
         {
-            return searchNode(v,mid+1,high,key);
+            return searchKey(v,mid+1,high,key);
         }
         else if(v[mid]->key>key)
         {
-            return searchNode(v,low,mid-1,key);
+            return searchKey(v,low,mid-1,key);
         }
         else
             return mid;
@@ -40,24 +56,26 @@ int searchNode(vector<node*> &v,int low,int high,int key)
 
 }
 
-void insertNode(vector<node*> &v,int key,int value)
+void insertkeyAndValue(vector<node*> &v,int key,int value)
 {
     int pos=0,i;
-    if(v.size()==0){
-    node *newNode=new node();
-    newNode->key=key;
-    newNode->value=value;
-    v.push_back(newNode);
+    key=hashKeyGeneration(convertInt(key));
+    if(v.size()==0)
+    {
+        node *newNode=new node();
+        newNode->key=key;
+        newNode->value=value;
+        v.push_back(newNode);
     }
 
-    i=searchNode(v,0,v.size(),key);
+    i=searchKey(v,0,v.size(),key);
 
 
     if(i==-1)
     {
-    node *newNode=new node();
-    newNode->key=key;
-    newNode->value=value;
+        node *newNode=new node();
+        newNode->key=key;
+        newNode->value=value;
         i = v.size()-1;
         while(key<v[i]->key && i>=0)
         {
@@ -73,12 +91,18 @@ void insertNode(vector<node*> &v,int key,int value)
 
 }
 
-bool deleteNode(vector<node*> &v,int key)
+bool deletekeyAndValue(vector<node*> &v,int key)
 {
+    int index=searchKey(v,0,v.size(), key=hashKeyGeneration(convertInt(key)));
+    if(index!=-1)
+    {
+        v.erase(v.begin()+index);
+    }
 
-        for(int i=0;i<v.size();i++){
+    for(int i=0; i<v.size(); i++)
+    {
         cout<<"key="<<v[i]->key<<" and value="<<v[i]->value<<endl;
-        }
+    }
 
     return true;
 }
@@ -87,9 +111,9 @@ int main()
 {
 
     vector<node *> map;
-    insertNode(map,1,10);
-    insertNode(map,4,12);
-    insertNode(map,3,13);
-    deleteNode(map,4);
+    insertkeyAndValue(map,1,10);
+    insertkeyAndValue(map,4,12);
+    insertkeyAndValue(map,3,13);
+    deletekeyAndValue(map,4);
     return 0;
 }
