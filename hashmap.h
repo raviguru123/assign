50ecd83afc706fba71a7ab31286e0ed520633f50
raviguru123 <raviguruiitr@gmail.com>
2016-09-19 20:02:04 +0530

    reorganize code

--- hashmap.h
+++ hashmap.h
@@ -72,6 +72,81 @@ bool searchAndDelete(node *head,int key)
     }
 
 }
+//label 1 function start from here
+void print(vector<node*>&v)
+{
+    for(int i=0; i<v.size(); i++)
+    {
+        while(v[i]->next!=NULL)
+        {
+            cout<<"key="<<v[i]->next->key<<" value="<<v[i]->next->value<<" ";
+            v[i]=v[i]->next;
+        }
+        cout<<endl;
+    }
+
+}
+
+int hashKeyGeneration(string word)
+{
+    int result = 0;
+    for(size_t i = 0; i < word.length(); ++i)
+    {
+        result += word[i] * pow(31, i);
+    }
+    return result;
+}
+
+string convertInt(int number)
+{
+    if (number == 0)
+        return "0";
+    string temp="";
+    string returnvalue="";
+    while (number>0)
+    {
+        temp+=number%10+48;
+        number/=10;
+    }
+    for (int i=0; i<temp.length(); i++)
+        returnvalue+=temp[temp.length()-i-1];
+    return returnvalue;
+}
+
+int searchKey(vector<node*> &v,int low,int high,int key)
+{
+    int mid=(low+high)/2;
+    if(low>=high)
+    {
+        return -1;
+    }
+    else
+    {
+        if(v[mid]->key<key)
+        {
+            return searchKey(v,mid+1,high,key);
+        }
+        else if(v[mid]->key>key)
+        {
+            return searchKey(v,low,mid-1,key);
+        }
+        else
+            return mid;
+    }
+
+}
+
+node * createMapNode(int indexKey,int key,int value)
+{
+    node *head=new node();
+    head->key=indexKey;
+    node *newNode=new node();
+    newNode->key=key;
+    newNode->value=value;
+    newNode->next=NULL;
+    head->next=newNode;
+    return head;
+}
 
 
 #endif // HASHMAP_H_INCLUDED
