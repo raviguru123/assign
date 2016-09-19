13f8b4e8789789dacde70869e57819993d9e06c5
raviguru123 <raviguruiitr@gmail.com>
2016-09-19 16:06:23 +0530

    next phase commit

--- main.cpp
+++ main.cpp
@@ -1,26 +1,55 @@
 #include <iostream>
 #include<vector>
 #include<algorithm>
+#include<math.h>
+#include<string>
 #include "hashmap.h"
 using namespace std;
 
-void insertNode(int key,int value){
-    node *head=new node();
-    head->next=NULL;
+template < class T >
+string to_string( T t )
+{
+    stringstream ss;
+    ss << t;
+    return ss.str();
+}
+
+
+int hashKeyGeneration(string word) {
+    int result = 0;
+    for(size_t i = 0; i < word.length(); ++i) {
+        result += word[i] * pow(31, i);
+    }
+    return result;
+}
+
+int searchNode(vector<node*> &v,int low,int high,int key){
+  int mid=(low+high)/2;
+    if(low>=high){
+        return -1;
+    }
+    else{
+            if(v[mid]->key<key){
+            return searchNode(v,mid+1,high,key);
+            }
+            else if(v[mid]->key>key){
+            return searchNode(v,low,mid-1,key);
+            }
+            else
+                return mid;
     }
 
-
-int searchNode(int key){
-
-    return 1;
     }
 
+void insertNode(vector<node*> &v,int key,int value){
+       int newkey=hashKeyGeneration(to_string(key));
+}
 
-int findIndex(int key){
+int findIndex(vector<node*> &v,int key){
 
 }
 
-bool deleteNode(int key){
+bool deleteNode(vector<node*> &v,int key){
 
 
 
@@ -31,6 +60,6 @@ int main()
 {
 
  vector<node *> map;
-
+     insertNode(map,1,2);
     return 0;
 }
